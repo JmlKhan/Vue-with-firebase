ec3a3cc8f1bf5bda3990aec8972d866925c61139
Jamal <jmlkhon17@gmail.com>
2021-11-08 14:16:22 +0500

    using axios

--- src/components/survey/LearningSurvey.vue
+++ src/components/survey/LearningSurvey.vue
@@ -50,6 +50,8 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
@@ -72,17 +74,25 @@ export default {
       //   rating: this.chosenRating,
       // });
 
-      fetch(
-        'https://vue-http-demo-f70f3-default-rtdb.firebaseio.com/surveys.json',
+      // fetch(
+      //   'https://vue-http-demo-f70f3-default-rtdb.firebaseio.com/surveys.json',
+      //   {
+      //     method: 'POST',
+      //     headers: {
+      //       'Content-Type': 'application/json',
+      //     },
+      //     body: JSON.stringify({
+      //       name: this.enteredName,
+      //       rating: this.chosenRating,
+      //     }),
+      //   }
+      // );
+
+      axios.post(
+        'https://vue-http-demo-f70f3-default-rtdb.firebaseio.com/survey.json',
         {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            name: this.enteredName,
-            rating: this.chosenRating,
-          }),
+          name: this.enteredName,
+          rating: this.chosenRating,
         }
       );
 
